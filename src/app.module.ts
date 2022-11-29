0cc50b40735697787df493410350f6038b0e7812
Agustin Gomez <agusleongomez@gmail.com>
2022-11-29 19:46:54 -0300

    connected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RmaterialModule } from './rmaterial/rmaterial.module';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { RmaterialModule } from './rmaterial/rmaterial.module'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
   imports: [
     RmaterialModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [AppController],
   providers: [AppService],

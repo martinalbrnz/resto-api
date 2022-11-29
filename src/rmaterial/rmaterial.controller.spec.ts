6b26ebd95bd7846ca8336808d7a1e5b243e6c9e2
Agustin Gomez <agusleongomez@gmail.com>
2022-11-29 12:03:47 -0300

    rMaterial resources created

---
+++ src/rmaterial/rmaterial.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RmaterialController } from './rmaterial.controller';
+import { RmaterialService } from './rmaterial.service';
+
+describe('RmaterialController', () => {
+  let controller: RmaterialController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RmaterialController],
+      providers: [RmaterialService],
+    }).compile();
+
+    controller = module.get<RmaterialController>(RmaterialController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});

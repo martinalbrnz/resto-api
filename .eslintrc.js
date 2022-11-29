e53cea5b7fe789257277d2c73b77855dad932c80
martinalbrnz <martinalbor96@gmail.com>
2022-11-29 20:37:50 -0300

    Fix conf so it fix errors on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    eqeqeq: ['warn', 'smart'],
+    'no-extra-semi': 'error',
+    semi: ['warn', 'never'],
+    'no-multiple-empty-lines': ['error'],
+    'eol-last': ['error'],
   },
-};
+}

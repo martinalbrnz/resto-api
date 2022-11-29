03d461cdf87a75275f9eb06d98b2e9911cbf15ca
Agustin Gomez <agusleongomez@gmail.com>
2022-11-29 14:26:18 -0300

    added .env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RmaterialModule } from './rmaterial/rmaterial.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RmaterialModule],
+  imports: [
+    RmaterialModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/rmaterial/rmaterial.controller.ts
+++ src/rmaterial/rmaterial.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RmaterialService } from './rmaterial.service';
 import { CreateRmaterialDto } from './dto/create-rmaterial.dto';
 import { UpdateRmaterialDto } from './dto/update-rmaterial.dto';
@@ -23,7 +31,10 @@ export class RmaterialController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRmaterialDto: UpdateRmaterialDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateRmaterialDto: UpdateRmaterialDto,
+  ) {
     return this.rmaterialService.update(+id, updateRmaterialDto);
   }
 

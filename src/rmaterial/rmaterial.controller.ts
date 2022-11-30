ce79825cf574816e95c2357248277616b400d47d
Agustin Gomez <agusleongomez@gmail.com>
2022-11-29 21:56:54 -0300

    endpoints created

--- src/rmaterial/rmaterial.controller.ts
+++ src/rmaterial/rmaterial.controller.ts
@@ -2,44 +2,66 @@ import {
   Controller,
   Get,
   Post,
+  Put,
   Body,
-  Patch,
-  Param,
   Delete,
-} from '@nestjs/common';
-import { RmaterialService } from './rmaterial.service';
-import { CreateRmaterialDto } from './dto/create-rmaterial.dto';
-import { UpdateRmaterialDto } from './dto/update-rmaterial.dto';
+  Res,
+  Param,
+  Query,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common'
+import { RmaterialService } from './rmaterial.service'
+import { CreateRmaterialDto } from './dto/create-rmaterial.dto'
+// import { UpdateRmaterialDto } from './dto/update-rmaterial.dto'
 
 @Controller('rmaterial')
 export class RmaterialController {
   constructor(private readonly rmaterialService: RmaterialService) {}
 
-  @Post()
-  create(@Body() createRmaterialDto: CreateRmaterialDto) {
-    return this.rmaterialService.create(createRmaterialDto);
+  @Get('/materials')
+  async findAll(@Res() res) {
+    const materials = await this.rmaterialService.findAll()
+    if (!materials) throw new NotFoundException('Raw Materials list is empty')
+    return res.status(HttpStatus.OK).json({
+      msg: 'Materials found successfully',
+      materials
+    })
   }
 
-  @Get()
-  findAll() {
-    return this.rmaterialService.findAll();
+  @Get('/:id')
+  async findOne(@Param('id') id: string, @Res() res) {
+    const material = await this.rmaterialService.findOne(id)
+    if (!material) throw new NotFoundException('Material does not exist')
+    return res.status(HttpStatus.OK).json(material)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rmaterialService.findOne(+id);
+  @Post('/create')
+  async create(@Res() res, @Body() createRmaterialDto: CreateRmaterialDto) {
+    const createdMaterial = await this.rmaterialService.create(createRmaterialDto)
+    return res.status(HttpStatus.OK).json({
+      msg: 'Material created successfully',
+      createdMaterial
+    })
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateRmaterialDto: UpdateRmaterialDto,
-  ) {
-    return this.rmaterialService.update(+id, updateRmaterialDto);
+  @Put('/update')
+  async update(@Res() res, @Body() createdRmaterialDto: CreateRmaterialDto, @Query('id') id:string) {
+    const updatedMaterial = await this.rmaterialService.update(id, createdRmaterialDto)
+    if (!updatedMaterial) throw new NotFoundException('Material does not exist')
+    return res.status(HttpStatus.OK).json({
+      msg: 'Material updated successfully',
+      updatedMaterial
+    })
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rmaterialService.remove(+id);
+  @Delete('/delete')
+  async remove(@Res() res, @Query('id') id) {
+    const deletedMaterial = await this.rmaterialService.remove(id)
+    if (!deletedMaterial) throw new NotFoundException('Material does not exist')
+    return res.status(HttpStatus.OK).json({
+      msg: 'Material deleted successfully',
+      deletedMaterial
+    })
   }
 }

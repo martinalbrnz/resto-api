141147fca05b3560327f9339b13f7f45c7a50a57
Agustin Gomez <agusleongomez@gmail.com>
2022-11-29 21:08:41 -0300

    rMaterial.dto and interface added

--- src/rmaterial/dto/update-rmaterial.dto.ts
+++ src/rmaterial/dto/update-rmaterial.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRmaterialDto } from './create-rmaterial.dto';
+import { PartialType } from '@nestjs/mapped-types'
+import { CreateRmaterialDto } from './create-rmaterial.dto'
 
 export class UpdateRmaterialDto extends PartialType(CreateRmaterialDto) {}

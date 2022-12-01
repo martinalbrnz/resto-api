1d961ee270490e85d67b17c80a35f85ec1e37b81
Agustin Gomez <agusleongomez@gmail.com>
2022-12-01 15:11:21 -0300

    created products resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service'
 import { RmaterialModule } from './rmaterial/rmaterial.module'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ProductsModule } from './products/products.module'
 
 @Module({
   imports: [
     RmaterialModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

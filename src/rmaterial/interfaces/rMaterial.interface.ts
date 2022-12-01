e3cb0577603cd544ed68ca06d5f21403013cd280
Agustin Gomez <agusleongomez@gmail.com>
2022-12-01 14:27:04 -0300

    add denomination to rMaterial structure

--- src/rmaterial/interfaces/rMaterial.interface.ts
+++ src/rmaterial/interfaces/rMaterial.interface.ts
@@ -3,6 +3,7 @@ import { Document } from 'mongoose'
 export interface RMaterial extends Document {
   readonly name: string,
   readonly amount: number,
+  readonly denomination: number,
   readonly cost: number,
   readonly entry_date: Date,
 }

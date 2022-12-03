095d0be53122b015c035b4197efb073f80f9e7be
Agustin Gomez <agusleongomez@gmail.com>
2022-12-03 06:24:04 -0300

    table services added

--- src/tables/tables.service.ts
+++ src/tables/tables.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 import { CreateTableDto } from './dto/create-table.dto'
 import { UpdateTableDto } from './dto/update-table.dto'
+import { Table, TableDocument } from './schemas/table.schema'
 
 @Injectable()
 export class TablesService {
-  create(createTableDto: CreateTableDto) {
-    return 'This action adds a new table'
-  }
+  constructor(@InjectModel(Table.name) private tableMethod: Model<TableDocument>) { }
 
-  findAll() {
-    return 'This action returns all tables'
+  findAll(): Promise<Table[]> {
+    const allTables = this.tableMethod.find().exec()
+    return allTables
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} table`
+    const table = this.tableMethod.findById(id).exec()
+    return table
+  }
+
+  async create(createTableDto: CreateTableDto) {
+    const createdTable = await new this.tableMethod(createTableDto).save()
+    return createdTable
   }
 
   update(id: string, updateTableDto: UpdateTableDto) {
-    return `This action updates a #${id} table`
+    const updatedTable = this.tableMethod.findByIdAndUpdate(id, updateTableDto, { new: true })
+    return updatedTable
   }
 
   remove(id: string) {
-    return `This action removes a #${id} table`
+    const deletedTable = this.tableMethod.findByIdAndDelete(id)
+    return deletedTable
   }
 }
